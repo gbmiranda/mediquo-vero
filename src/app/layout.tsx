d509bc3b0fec6426ddc765809d73df59f53242cc
Gabriel Braga <gbmiranda@me.com>
2025-08-20 09:44:58 -0300

    Update Google Tag Manager container ID
    Changed the GTM container ID from GTM-NMD7QJ2F to GTM-W9DRRQD2 in both script and noscript sections to use the new tracking configuration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NMD7QJ2F');`
+})(window,document,'script','dataLayer','GTM-W9DRRQD2');`
         }} />
         {/* End Google Tag Manager */}
         <meta name="generator" content="v0.dev" />
@@ -250,7 +250,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </head>
       <body className={montserrat.className}>
         {/* Google Tag Manager (noscript) */}
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NMD7QJ2F"
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W9DRRQD2"
         height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
         {/* End Google Tag Manager (noscript) */}
         <GoogleAnalytics />
